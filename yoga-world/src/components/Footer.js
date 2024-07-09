76c6168427d85483e05c92ebf8cdea9fad32db91
faekhatami <faekhatami@gmail.com>
2024-07-09 18:30:27 +0330

    footer

---
+++ yoga-world/src/components/Footer.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className="container">
+        <p>&copy; 2024 Yoga World. All rights reserved</p>
+        <nav>
+          <ul>
+            <li>
+              <a href="#privacy"></a>
+            </li>
+            <li>
+              <a href="#terms"></a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
