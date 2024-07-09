678aabf4db31224f3b2c564673236dfb9c8f2908
faekhatami <faekhatami@gmail.com>
2024-07-09 18:19:13 +0330

    header

---
+++ yoga-world/src/components/Header.js
@@ -0,0 +1,32 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header>
+      <div className="container">
+        <h1>Yoga World 🧘🏻‍♀️</h1>
+        <nav>
+          <ul>
+            <li>
+              <a href="#home">Home</a>
+            </li>
+            <li>
+              <a href="about">About</a>
+            </li>
+            <li>
+              <a href="#articles">Articles</a>
+            </li>
+            <li>
+              <a href="#contact">Contact</a>
+            </li>
+            <li>
+              <a href="#api-data">API Data</a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
