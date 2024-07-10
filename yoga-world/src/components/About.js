a13ff36efe6d95f4380d508a6bd82634277e721c
faekhatami <faekhatami@gmail.com>
2024-07-10 08:25:18 +0330

    about

---
+++ yoga-world/src/components/About.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section id="about">
+      <h2>About This Blog</h2>
+      <p>Learn more about the mission of this blog and the author behind it.</p>
+    </section>
+  );
+};
+
+export default About;
