3369c91b06e22232cad251fba45c69f214a211a3
faekhatami <faekhatami@gmail.com>
2024-07-12 14:23:54 +0330

    contact form

---
+++ yoga-world/src/components/ContactForm.js
@@ -0,0 +1,78 @@
+// src/components/ContactForm.js
+import React, { useState } from "react";
+
+const ContactForm = () => {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+
+  const [isSubmitted, setIsSubmitted] = useState(false);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (formData.name && formData.email && formData.message) {
+      console.log("Form Submitted!", formData);
+      alert("Thank you for your message!");
+      setIsSubmitted(true);
+      setFormData({
+        name: "",
+        email: "",
+        message: "",
+      });
+    } else {
+      alert("Please fill in all fields.");
+    }
+  };
+
+  return (
+    <section id="contact">
+      <h2>Contact</h2>
+      <p>Feel free to reach out with any questions or comments.</p>
+      {isSubmitted && <p>Thank you for your message!</p>}
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="name">Name:</label>
+        <input
+          type="text"
+          id="name"
+          name="name"
+          value={formData.name}
+          onChange={handleChange}
+          required
+        />
+
+        <label htmlFor="email">Email:</label>
+        <input
+          type="email"
+          id="email"
+          name="email"
+          value={formData.email}
+          onChange={handleChange}
+          required
+        />
+
+        <label htmlFor="message">Message:</label>
+        <textarea
+          name="message"
+          id="message"
+          value={formData.message}
+          onChange={handleChange}
+          required
+        ></textarea>
+
+        <button type="submit">Send</button>
+      </form>
+    </section>
+  );
+};
+
+export default ContactForm;
