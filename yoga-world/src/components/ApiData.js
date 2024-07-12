0a18a0c935bf295ceb1747bd57a39804f5f44f52
faekhatami <faekhatami@gmail.com>
2024-07-12 14:48:14 +0330

    api data

--- yoga-world/src/components/ApiData.js
+++ yoga-world/src/components/ApiData.js
@@ -1,34 +1,44 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 const ApiData = () => {
   const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(false);
 
-  const fetchDataUsingAsyncAwait = async () => {
+  const fetchData = async () => {
+    setLoading(true);
     try {
       const response = await fetch(
         "https://jsonplaceholder.typicode.com/posts"
       );
-      const data = await response.json();
-      setData(data);
+      const result = await response.json();
+      setData(result);
     } catch (error) {
       console.error("Error fetching data:", error);
+    } finally {
+      setLoading(false);
     }
   };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
 
   return (
     <section id="api-data">
       <h2>Fetched API Data</h2>
-      <div id="data-container">
-        {data.map((item) => (
-          <div key={item.id} className="data-item">
-            <h3>{item.title}</h3>
-            <p>{item.body}</p>
-          </div>
-        ))}
-      </div>
-      <button id="fetch-data-btn" onClick={fetchDataUsingAsyncAwait}>
-        Fetch Data
-      </button>
+      {loading ? (
+        <p>Loading...</p>
+      ) : (
+        <div id="data-container">
+          {data.map((item) => (
+            <div key={item.id} className="data-item">
+              <h3>{item.title}</h3>
+              <p>{item.body}</p>
+            </div>
+          ))}
+        </div>
+      )}
+      <button onClick={fetchData}>Fetch Data</button>
     </section>
   );
 };
